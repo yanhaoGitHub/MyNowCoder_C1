69ceae92f191f4a479479501cc774b199f522ba7
yanhao <2683751052@qq.com>
2016-06-29 19:33:06 +0800

    after err

--- Test/decorator.py
+++ Test/decorator.py
@@ -1,2 +1,14 @@
 #-*- encoding=UTF-8 -*-
+def log(func):
+    def wrapper():
+        print "before calling", func.__name__
+        func()
+        print "end calling", func.__name__
+    return wrapper
 
+@log
+def hello():
+    print "enter&code&here"
+
+if __name__ == "__main__":
+    hello()
